fb456e1731a1090e87326bb522818eed50349f55
Glitch (alex-client-project) <none>
2022-05-25 22:30:21 +0000

    🥲💌 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 /* global $ */
 
-console.log('hi');
-
+//console.log('hi');
+//
 $('h1').click(function(){
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
 });
+
+let hpAPIurl="https://www.googleapis.com/books/v1/volumes?q=harry%20potter";
+console.log(hpAPIurl);
+fetch(hpAPIurl);
+
+let userInput=$("input").val(); 
+
+
+
+
+
+
